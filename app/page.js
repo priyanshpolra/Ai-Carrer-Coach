56373b9f1a9f86b2af61324020cdb78f380f982f
priyanshpolra <priyanshpolra34@gmail.com>
2025-07-19 11:33:05 +0530

    Clerk Auth

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       PolarAi
+      <br/>
+      <Button>Hello</Button>
     </div>
   );
 }
