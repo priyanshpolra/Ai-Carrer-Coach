5a374e376ec4492fd4684b18726c193bcc8258ca
priyanshpolra <priyanshpolra34@gmail.com>
2025-07-19 23:38:01 +0530

    Ui and Database

--- components/header.jsx
+++ components/header.jsx
@@ -4,8 +4,10 @@ import React from 'react'
 import { Button } from './ui/button'
 import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarIcon } from 'lucide-react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { checkUser } from '@/lib/checkUser'
 
-const Header = () => {
+const Header = async () => {
+    await checkUser()
     return (
         <header className='fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
             <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
