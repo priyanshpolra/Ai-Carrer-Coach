489d96ae30324d7abc53f3713538e5459fd2151f
priyanshpolra <priyanshpolra34@gmail.com>
2025-07-21 18:42:45 +0530

    Inngest Completion

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,18 +1,21 @@
+import { getIndustryInsights } from '@/action/dashboard'
 import { getUserOnboardingStatus } from '@/action/user'
 import { redirect } from 'next/navigation'
 import React from 'react'
+import DashboardView from './_components/dashboard-view'
 
 const IndustryInsightsPage = async () => {
 
     const {isOnboarded} = await getUserOnboardingStatus()
+    const insights = await getIndustryInsights()
     
         if(!isOnboarded){
             redirect("/onboarding")
         }
 
     return (
-        <div>
-            Industry Insights Page
+        <div className='container mx-auto'>
+            <DashboardView insights={insights}/>
         </div>
     )
 }
